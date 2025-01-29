5eea446f7800fccc215a678aee77c588c1ed5085
Supun-Jayarathne <jayarathnesnj@gmail.com>
2025-01-30 00:30:38 +0530

    Removed some unwanted code lines

--- extract_report_agent/src/extract_report_agent/main.py
+++ extract_report_agent/src/extract_report_agent/main.py
@@ -1,19 +1,10 @@
-#!/usr/bin/env python
-import sys
 import warnings
 import PyPDF2
 import gradio as gr
-
-from datetime import datetime
-
 from extract_report_agent.crew import ExtractReportAgent
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
 topic = ''
 
 def run():
@@ -22,50 +13,12 @@ def run():
     """
     inputs = {
         'topic': topic,
-        # 'current_year': str(datetime.now().year)
     }
     
     try:
         ExtractReportAgent().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
-
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        'topic': topic,
-    }
-    try:
-        ExtractReportAgent().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        ExtractReportAgent().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        'topic': topic,
-    }
-    try:
-        ExtractReportAgent().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
 
 def read_cv_content(file_path):
     with open(file_path, 'rb') as file:
